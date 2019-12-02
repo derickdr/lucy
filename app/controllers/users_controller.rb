ddec612281aaa68445c65ee91fcf1ed21ac4cab5
Derick Del Rosario <derickmitchel@icloud.com>
2019-12-02 14:05:10 -0800

    core controllers

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def show_cart
+  end
+
+  def edit
+  end
+
+  def edit_cart
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def delete
+  end
+end
