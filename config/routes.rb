45a60536b07b7e20ef2d34268b6dee664d5e7a96
Derick Del Rosario <derickmitchel@icloud.com>
2019-12-10 09:44:58 -0800

    home/index as root

--- config/routes.rb
+++ config/routes.rb
@@ -41,6 +41,6 @@ Rails.application.routes.draw do
   get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
-  get 'home/index'
+  get 'home/index', as: 'root'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
